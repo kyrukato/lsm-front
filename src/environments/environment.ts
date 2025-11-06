e4ec32f86bfc9d0eb058ae77af8ed636381309f5
kyrukato <167032913+kyrukato@users.noreply.github.com>
2025-11-06 13:34:14 -0700

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,11 @@
 export const environment = {
-  URL_AUTH: 'https://backlsm-production.up.railway.app/auth',
-  URL_DICTIONARY: 'https://backlsm-production.up.railway.app/dictionary',
-  URL_GUESS_LOCAL: 'https://backlsm-production.up.railway.app/guess-local',
-  URL_SEQUENCE_LOCAL: 'https://backlsm-production.up.railway.app/sequence-local',
-  URL_MEMORY_LOCAL: 'https://backlsm-production.up.railway.app/memory-local',
+  URL_AUTH: 'https://backlsm-yctc.onrender.com/auth',
+  URL_DICTIONARY: 'https://backlsm-yctc.onrender.com/dictionary',
+  URL_GUESS_LOCAL: 'https://backlsm-yctc.onrender.com/guess-local',
+  URL_SEQUENCE_LOCAL: 'https://backlsm-yctc.onrender.com/sequence-local',
+  URL_MEMORY_LOCAL: 'https://backlsm-yctc.onrender.com/memory-local',
+  URL_GUESS_PVP: 'https://backlsm-yctc.onrender.com/guess-pvp',
+  URL_SEQUENCE_PVP: 'https://backlsm-yctc.onrender.com/sequence-pvp',
+  URL_MEMORY_PVP: 'https://backlsm-yctc.onrender.com/memory-pvp',
   API_GEMINI: 'AIzaSyAmMOpbDGVAKj_XkZAkElOBeEJfkJtEzXA',
 };
